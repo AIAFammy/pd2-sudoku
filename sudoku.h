22424d6f08aa3b4fdc40e11cdc093ad1b43811ab
C14011277 <ming830710@gmail.com>
2016-03-24 01:36:57 +0800

    declare 9 init. fuc. name

---
+++ sudoku.h
@@ -0,0 +1,17 @@
+
+class Sudoku{
+  public:
+    void giveQuestion();
+    void readIn();
+    void solve();
+    void changeNum(int a, int b);
+    void changeRow(int a, int b);
+    void changeCol(int a, int b);
+    void rotate(int n);
+    void flip(int n);
+    void transform();
+
+  private:
+    int map[81];
+
+}  
